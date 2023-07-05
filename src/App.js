1fbe9f1ce559aa8874cba9cd4e35975c99e20a68
Gloria-Lado <glorialadoo@gmail.com>
2023-07-05 12:51:27 +0300

    files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Login from './Login';
+import './App.css';
+import Products from './Products';
+
+function App() {
+  return (
+    <div>
+     <Login></Login>
+     <Products/>
+    </div>
+  );
+}
+
+export default App;
